2d27b55ab5dfffa54e40239a19e1eac367b5e4f2
fernandomoraes <fernandosdemoraes@gmail.com>
2018-10-14 19:27:03 -0300

    Add tests

--- fail-safe-docker/src/main/java/com/fndomoraes/User.java
+++ fail-safe-docker/src/main/java/com/fndomoraes/User.java
@@ -1,4 +1,43 @@
 package com.fndomoraes;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "USERS")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    protected User() {
+    }
+
+    User(final String name, final String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    Long getId() {
+        return id;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getEmail() {
+        return email;
+    }
+
+    void setName(final String name) {
+        this.name = name;
+    }
 }
---
+++ fail-safe-docker/src/test/java/com/fndomoraes/UserPersistanceTestIT.java
@@ -0,0 +1,91 @@
+package com.fndomoraes;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import java.util.function.Consumer;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertNull;
+
+public class UserPersistanceTestIT {
+
+    private static EntityManagerFactory emf;
+
+    @BeforeClass
+    public static void beforeClass() {
+        emf = Persistence.createEntityManagerFactory("FailSafeDocker");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        emf.close();
+    }
+
+    @Before
+    public void before() {
+        doInTransaction(em -> {
+            em.createQuery("DELETE FROM User").executeUpdate();
+        });
+    }
+
+    @Test
+    public void should_persist_a_new_user() {
+        final User user = new User("bill", "bill@bill.com");
+
+        doInTransaction(em -> {
+            em.persist(user);
+        });
+
+        assertNotNull(user.getId());
+
+        doInTransaction(em -> {
+            final User persisted = em.find(User.class, user.getId());
+            assertEquals(user.getName(), persisted.getName());
+            assertEquals(user.getEmail(), persisted.getEmail());
+        });
+    }
+
+    @Test
+    public void should_update_a_user() {
+        final User user = new User("bill", "bill@bill.com");
+
+        doInTransaction(em -> em.persist(user));
+
+        doInTransaction(em -> em.find(User.class, user.getId()).setName("bill2"));
+
+        doInTransaction(em -> assertEquals("bill2", em.find(User.class, user.getId()).getName()));
+    }
+
+    @Test
+    public void should_remove_a_user() {
+        final User user = new User("bill", "bill@bill.com");
+
+        doInTransaction(em -> em.persist(user));
+
+        doInTransaction(em -> assertNotNull(em.find(User.class, user.getId())));
+
+        doInTransaction(em -> em.remove(em.find(User.class, user.getId())));
+
+        doInTransaction(em -> assertNull(em.find(User.class, user.getId())));
+    }
+
+    private static void doInTransaction(final Consumer<EntityManager> consumer) {
+        final EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            consumer.accept(em);
+            em.getTransaction().commit();
+        } catch (final Exception exception) {
+            em.getTransaction().rollback();
+            throw exception;
+        }
+    }
+}
